ee46e06f8b2d1c755c904f906a9c44c9c58196e7
VACACELA SACA JHOSTIN OMAR <103053087+jhos-vac@users.noreply.github.com>
2024-05-29 11:35:53 -0500

    init commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Neo4jModule } from './neo4j/neo4j.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Neo4jConfig } from './neo4j/neo4j-config.interface';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { EncryptionModule } from './encryption/encryption.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { GenreModule } from './genre/genre.module';
+import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    Neo4jModule.forRootAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService, ],
+      useFactory: (configService: ConfigService) : Neo4jConfig => ({
+        scheme: configService.get('NEO4J_SCHEME'),
+        host: configService.get('NEO4J_HOST'),
+        port: configService.get('NEO4J_PORT'),
+        username: configService.get('NEO4J_USERNAME'),
+        password: configService.get('NEO4J_PASSWORD'),
+        database: configService.get('NEO4J_DATABASE'),
+      })
+    }),
+    AuthModule,
+    UserModule,
+    EncryptionModule,
+    SubscriptionModule,
+    GenreModule,
+    CheckoutModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
